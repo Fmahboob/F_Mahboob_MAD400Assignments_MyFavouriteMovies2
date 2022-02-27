5873ea5e404deab236caec45cc0782e35cfd15d2
Fmahboob <91558787+Fmahboob@users.noreply.github.com>
2022-02-27 15:01:02 -0500

    Create a private array, constructor, getter, function that will add new movie, function that will return number of items and function that has all details about movie

---
+++ F_Mahboob_MyFavoutieMovies/src/app/helper-files/content-list.ts
@@ -0,0 +1,39 @@
+import { Console } from "console";
+import { Content } from "./content-interface";
+
+class ContentList{
+    private _items: Content[] = [];
+    
+    static count = 0;
+    detail = "";
+
+    Constructor(){
+        this._items = [];
+    }
+
+    get items(): Content[] {
+        return this._items;
+    }
+
+    addMovie(newMovie: Content){
+        this._items.push(newMovie)
+        this.noOfItems
+
+    }
+
+    noOfItems(){
+        return ++ContentList.count;
+    }
+
+    movieInformation(myMovie: Content): string{
+     this.detail = "<h4>" + myMovie.title + " is my Favourite movie. Poster of movie is " + myMovie.imgURL + " The director of this movie is " + myMovie.creator + ". This description of this movie is " + myMovie.description + ". This is recomended for " + myMovie.tags
+     return this.detail;
+
+    }
+
+   
+}
+
+ 
+
+
